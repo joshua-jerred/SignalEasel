d0bc03b1c8091e0e9d18fd573ec09fd407340132
Joshua Jerred <19292194+joshua-jerred@users.noreply.github.com>
2024-10-12 14:59:23 -0500

    Code Coverage with gcovr

---
+++ project/coverage.sh
@@ -0,0 +1,18 @@
+mkdir -p build
+cd build
+
+# Delete old coverage file if it exists
+rm -f coverage_data.*
+
+# Configure
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DSIGNALEASEL_COVERAGE=ON
+
+# Build
+cmake --build . --target signal_easel_unit_tests
+
+# Run the unit tests
+ctest --force-new-ctest-process
+
+# Generate coverage report. Create an LCOV report file and print a text summary
+gcovr -r .. --config ../project/gcovr.cfg --lcov coverage_data.lcov --txt coverage_data.txt
+cat coverage_data.txt

c9edb7218b4682e4c8c772e9a35df4026bf6b94f
Joshua Jerred <19292194+joshua-jerred@users.noreply.github.com>
2024-10-12 19:03:52 -0500

    test coverage complete

--- project/coverage.sh
+++ project/coverage.sh
@@ -13,11 +13,13 @@ cmake .. -DCMAKE_BUILD_TYPE=Debug -DSIGNALEASEL_COVERAGE=ON
 cmake --build . --target signal_easel_unit_tests
 
 # Run the unit tests
-ctest --force-new-ctest-process
+ctest --force-new-ctest-process --quiet
 
 # Generate coverage report. Create an LCOV report file and print a text summary
 gcovr -r .. --config ../project/gcovr.cfg --lcov coverage_data.lcov --txt coverage_data.txt
-# cat coverage_data.txt
+
+# Print the report
+cat coverage_data.txt
 
 # COVERAGE_VAR = "TEST_COVERAGE=58"
 export COVERAGE_VAR="TEST_COVERAGE=$(tail -2 coverage_data.txt | head -1 | awk '{print $(NF)}' | tr -d '%')"
